3975c6153c4da96e1186092403ef6395e595a5e2
Banana0110 <vip.smiles@list.ru>
2022-02-01 00:02:50 +0600

    ??

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi Raatbek");
         System.out.println("hi sezim");
+        System.out.println("When we'll start?");
     }
 }
