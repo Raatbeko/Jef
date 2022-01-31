28ed29a6c39dd6fe0842f95f05af4bb407e6b057
Banana0110 <vip.smiles@list.ru>
2022-02-01 00:40:01 +0600

    So this code included type of airplanes, and also I've create the aircraft.

---
+++ src/AirPort/Airbus320.java
@@ -0,0 +1,21 @@
+package AirPort;
+
+public class Airbus320 extends AbstractAircraft {
+
+    //first type of airplane
+    public Airbus320() {
+        count++;
+        this.id += count;
+        this.model = this.getClass().getSimpleName();
+        this.type = "Citizens";
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus320{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
