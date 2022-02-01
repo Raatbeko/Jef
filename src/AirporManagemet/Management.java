41bc5534d2549dcb1883146ef5a7cd063ff3a3cc
Dell <jumabekovradik@gmail.com>
2022-02-01 19:18:07 +0600

    Flight management

---
+++ src/AirporManagemet/Management.java
@@ -0,0 +1,11 @@
+package AirporManagemet;
+
+public interface Management {
+
+    void addFlight();
+    void buyTicketOfNumberFlight();
+    void showAllFlight();
+    void report();
+    void searchFlightByTicketNumber();
+
+}
