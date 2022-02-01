a55cfc84130e7c1ed2abd63da3172d0d8771d9a6
Dell <jumabekovradik@gmail.com>
2022-02-01 19:18:07 +0600

    Flight management

---
+++ src/AirporManagemet/AirportManagement.java
@@ -0,0 +1,30 @@
+package AirporManagemet;
+
+public class AirportManagement implements Management {
+
+
+    @Override
+    public void addFlight() {
+
+    }
+
+    @Override
+    public void buyTicketOfNumberFlight() {
+
+    }
+
+    @Override
+    public void showAllFlight() {
+
+    }
+
+    @Override
+    public void report() {
+
+    }
+
+    @Override
+    public void searchFlightByTicketNumber() {
+
+    }
+}
